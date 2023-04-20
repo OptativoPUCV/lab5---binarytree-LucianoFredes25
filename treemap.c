fca007c732b20453fa6a25c3c630f9d4379b3447
Replit user <>
2023-04-20 16:29:45 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -76,7 +76,7 @@ Pair * searchTreeMap(TreeMap * tree, void* key) {
   TreeNode * aux = (TreeNode *)malloc(sizeof(TreeNode));
   aux = tree->root;
   while(true){
-    printf("%p\n", aux->pair->key);
+    
     if(aux == NULL)
       return NULL;
     if(aux->pair->key == key){
