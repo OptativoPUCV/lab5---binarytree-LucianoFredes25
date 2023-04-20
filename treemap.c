6569f9829280d4aa5593cdd21042075c2f46f5cc
Replit user <>
2023-04-20 18:11:46 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -46,7 +46,7 @@ TreeMap * createTreeMap(int (*lower_than) (void* key1, void* key2)) {
 }
 
 
-/*void insertTreeMap(TreeMap * tree, void* key, void * value) {
+void insertTreeMap(TreeMap * tree, void* key, void * value) {
   tree->current = tree->root;
 
   if(tree->root == NULL){
@@ -84,49 +84,7 @@ TreeMap * createTreeMap(int (*lower_than) (void* key1, void* key2)) {
       //printf("4");
       tree->current = tree->current->left;
     }
-  }*/
-  void insertTreeMap(TreeMap* tree, void* key, void* value){
-    // Step 1: Insertion into an empty tree
-    if (tree->root == NULL) {
-        tree->root = new TreeNode;
-        tree->root->pair = new Pair(key, value);
-        tree->root->left = NULL;
-        tree->root->right = NULL;
-        tree->root->parent = NULL;
-        tree->current = tree->root;
-        return;
-    }
-    
-    // Step 2: Search for the position to insert the new node
-    TreeNode* node = tree->root;
-    TreeNode* parent = NULL;
-    while (node != NULL) {
-        parent = node;
-        if (tree->lower_than(key, node->pair->key)) {
-            node = node->left;
-        } else if (tree->lower_than(node->pair->key, key)) {
-            node = node->right;
-        } else {
-            // Key already exists, just update the value
-            node->pair->value = value;
-            tree->current = node;
-            return;
-        }
-    }
-    
-    // Step 3: Insert the new node
-    TreeNode* new_node = new TreeNode;
-    new_node->pair = new Pair(key, value);
-    new_node->left = NULL;
-    new_node->right = NULL;
-    new_node->parent = parent;
-    if (tree->lower_than(key, parent->pair->key)) {
-        parent->left = new_node;
-    } else {
-        parent->right = new_node;
-    }
-    tree->current = new_node;
-}
+  }
 }
 
 
