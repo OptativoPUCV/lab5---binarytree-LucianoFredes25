05620a565cc3a7f52b12dc743775257dfdac5ae1
Replit user <>
2023-04-22 15:19:09 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -180,7 +180,7 @@ Pair * upperBound(TreeMap * tree, void* key) {
       if(tree->lower_than(key , tree->current->pair->key))
         return tree->current->pair->key;
       else
-        tree->current = nextTreeMap(tree);
+        tree->current->pair = nextTreeMap(tree);
   }
 }
 
